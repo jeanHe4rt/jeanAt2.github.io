539447919bd8023bfe6b796d1767e4345a4b73a5
jeanat2 <jean0king@gmail.com>
2022-02-08 21:29:45 -0300

    Erro de largura ok

--- jean.io/src/App.js
+++ jean.io/src/App.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 import Header from './components/Header/header.jsx'
 import Navbar from './components/Navbar/navbar.jsx';
-=======
-import Home from './components/pages/home-page.jsx'
->>>>>>> 9acaa76ca38775ed2c60a011446e87b276337e2f
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Navbar/>
       <Header />
-=======
-
->>>>>>> 9acaa76ca38775ed2c60a011446e87b276337e2f
     </div>
   );
 }
