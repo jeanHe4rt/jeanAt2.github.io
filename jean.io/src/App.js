569dde2e85506e16f67b1217a98ec1a0bea445ad
jeanat2 <jean0king@gmail.com>
2022-02-10 21:31:13 -0300

    Adicionando o AboutMe e Contacts

--- jean.io/src/App.js
+++ jean.io/src/App.js
@@ -1,11 +1,15 @@
 import Header from './components/Header/header.jsx'
 import Navbar from './components/Navbar/navbar.jsx';
+import AboutMe from './components/AboutMe/aboutMe.jsx';
+import Contacts from './components/Contacts/contacts.jsx';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Header />
+      <AboutMe />
+      <Contacts />
     </div>
   );
 }
