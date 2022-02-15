fe256705ab605ca6101fd1f773750ef1ae20bd0a
jeanat2 <jean0king@gmail.com>
2022-02-14 23:19:48 -0300

    Adicionado minhas Habilidades

--- jean.io/src/App.js
+++ jean.io/src/App.js
@@ -4,6 +4,7 @@ import AboutMe from './components/AboutMe/aboutMe.jsx';
 import Contacts from './components/Contacts/contacts.jsx';
 import Projects from './components/Projetos/projects.jsx';
 import Services  from './components/Serviços/servicos.jsx';
+import Skils  from './components/Skils/skils.jsx';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Contacts />
       <Projects />
       <Services />
+      <Skils />
     </div>
   );
 }
