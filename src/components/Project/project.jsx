1e432dabcd2b18eadebdf23c53ad133ec80d59b5
jeanAt2 <jean0king@gmail.com>
2022-02-21 20:36:47 -0300

    fix(Refatoração de Codigo): :bug: Arumando as variáveis do project

--- src/components/Project/project.jsx
+++ src/components/Project/project.jsx
@@ -4,15 +4,16 @@ import './project.css';
 import JavaScript from '../../assets/img/javascript.png';
 
 const Project = () => {
-  const title = "Titulo do Projeto";
+  
   const listTechs = "HTML, CSS e JS.";
-
+  const title = "Titulo do Projeto";
+  
   return( 
     <div className="projects">
       <figure className="project">
           <img src={JavaScript} alt=""/>
-          <figcaption className="figCaption-2">${title}</figcaption>
-          <label className="small-text-2">Tecnologias: ${listTechs} </label>
+          <figcaption className="figCaption-2"> { title } </figcaption>
+          <label className="small-text-2">Tecnologias: { listTechs } </label>
         </figure>
     </div>
   );
